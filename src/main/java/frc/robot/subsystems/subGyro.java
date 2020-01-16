a25aeda85858831f7cbabbfaa07b3ab6151e06b3
schwjef <jeff.schwennesen@elektrobit.com>
2020-01-16 13:57:59 -0500

    formatted it to look pretty

--- src/main/java/frc/robot/subsystems/subGyro.java
+++ src/main/java/frc/robot/subsystems/subGyro.java
@@ -4,8 +4,6 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.SPI;
 import com.kauailabs.navx.frc.AHRS;
 
-
-
 // import frc.robot.CommonLogic;
 // import frc.robot.Settings;
 
@@ -15,8 +13,8 @@ public class subGyro extends Subsystem {
   public static double navxOffset = 0.0;
 
   public subGyro() {
-
     navx = new AHRS(SPI.Port.kMXP);
+    resetNavx();
   }
 
   @Override
@@ -26,78 +24,69 @@ public class subGyro extends Subsystem {
 
   @Override
   public void periodic() {
-    
+
   }
 
   public double getNavxAngleRaw() {
-		return navx.getAngle();
-	}
+    return navx.getAngle();
+  }
 
-	public double getNormaliziedNavxAngle() {
-		return gyroNormalize(getNavxAngleRaw() + navxOffset);
-	}
+  public double getNormaliziedNavxAngle() {
+    return gyroNormalize(getNavxAngleRaw() + navxOffset);
+  }
 
-	public void resetNavx() {
-		navx.reset();
-		navx.zeroYaw();
-	}
+  public void resetNavx() {
+    navx.reset();
+    navx.zeroYaw();
+  }
 
-	public double gyroNormalize(double heading) {
-		// takes the full turns out of heading
-		// gives us values from 0 to 180 for the right side of the robot
-		// and values from 0 to -179 degrees for the left side of the robot
+  public double gyroNormalize(double heading) {
+    // takes the full turns out of heading
+    // gives us values from 0 to 180 for the right side of the robot
+    // and values from 0 to -179 degrees for the left side of the robot
+    double degrees = heading % 360;
 
-		double degrees = heading % 360;
+    if (degrees > 180) {
+      degrees = degrees - 360;
+    }
+    if (degrees < -179) {
+      degrees = degrees + 360;
+    }
+    return degrees;
+  }
 
-		if (degrees > 180) {
-			degrees = degrees - 360;
-		}
+  public boolean gyroInTol(double currHeading, double desiredHeading, double tol) {
 
-		if (degrees < -179) {
-			degrees = degrees + 360;
-		}
-
-		return degrees;
-	}
-
-	public boolean gyroInTol(double currHeading, double desiredHeading, double tol) {
-
-		double upperTol = gyroNormalize(desiredHeading + tol);
-		double lowerTol = gyroNormalize(desiredHeading - tol);
-		double normalCurr = gyroNormalize(currHeading);
-
-		double signumUpperTol = Math.signum(upperTol);
-		double signumLowerTol = Math.signum(lowerTol);
-
-		boolean retValue = false;
-		// works for all positive numbers direction values
-		if (signumUpperTol > 0 && signumLowerTol > 0) {
-			if ((normalCurr >= lowerTol) && (normalCurr <= upperTol)) {
-				retValue = true;
-			}
-		}
-
-		// works for negative values
-		else if (signumUpperTol < 0 && signumLowerTol < 0) {
-			if ((normalCurr >= lowerTol) && (normalCurr <= upperTol)) {
-				retValue = true;
-			}
-		}
-		// mixed values -tol to + tol This happens at 180 degrees
-		else if ((signumUpperTol < 0) && (signumLowerTol > 0)) {
-			if ((Math.abs(normalCurr) >= Math.abs(lowerTol)) && (Math.abs(normalCurr) >= Math.abs(upperTol))) {
-				retValue = true;
-			}
-
-		}
-		// mixed values -tol to + tol This happens at 0 degrees
-		else if ((signumUpperTol > 0) && (signumLowerTol < 0)) {
-			if ((Math.abs(normalCurr) <= Math.abs(lowerTol)) && (Math.abs(normalCurr) <= Math.abs(upperTol))) {
-				retValue = true;
-			}
-
-		}
-		return (retValue);
-	}
-
+    double upperTol = gyroNormalize(desiredHeading + tol);
+    double lowerTol = gyroNormalize(desiredHeading - tol);
+    double normalCurr = gyroNormalize(currHeading);
+    double signumUpperTol = Math.signum(upperTol);
+    double signumLowerTol = Math.signum(lowerTol);
+    boolean retValue = false;
+    // works for all positive numbers direction values
+    if (signumUpperTol > 0 && signumLowerTol > 0) {
+      if ((normalCurr >= lowerTol) && (normalCurr <= upperTol)) {
+        retValue = true;
+      }
+    }
+    // works for negative values
+    else if (signumUpperTol < 0 && signumLowerTol < 0) {
+      if ((normalCurr >= lowerTol) && (normalCurr <= upperTol)) {
+        retValue = true;
+      }
+    }
+    // mixed values -tol to + tol This happens at 180 degrees
+    else if ((signumUpperTol < 0) && (signumLowerTol > 0)) {
+      if ((Math.abs(normalCurr) >= Math.abs(lowerTol)) && (Math.abs(normalCurr) >= Math.abs(upperTol))) {
+        retValue = true;
+      }
+    }
+    // mixed values -tol to + tol This happens at 0 degrees
+    else if ((signumUpperTol > 0) && (signumLowerTol < 0)) {
+      if ((Math.abs(normalCurr) <= Math.abs(lowerTol)) && (Math.abs(normalCurr) <= Math.abs(upperTol))) {
+        retValue = true;
+      }
+    }
+    return (retValue);
+  }
 }
