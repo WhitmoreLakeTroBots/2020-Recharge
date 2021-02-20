d44a465035586019f92721926421ed6cb5a213e9
TroBots <wlhsroboticsteam@gmail.com>
2021-02-20 14:29:24 -0500

    DZ working commit

--- src/main/java/frc/robot/commands/autoGroupBang.java
+++ src/main/java/frc/robot/commands/autoGroupBang.java
@@ -15,9 +15,11 @@ public class autoGroupBang extends CommandGroup {
    * move segments and is thus smooth accurate and fast.
    */
   public autoGroupBang() {
-    double speedSlow = 0.2;
-    double speedMed = 0.4;
-    double speedFast = 0.7;
+    double speedSlow = 0.3;
+    double speedMed = 0.5;
+    double speedFast = 0.8;
+    double insidek = .13;
+    double outsidek = .5;
     
     // set a speed just fast enough to not be coasting
     double speedBrake = -0.01;
@@ -29,13 +31,13 @@ public class autoGroupBang extends CommandGroup {
     addSequential(new Auto_BangDriveByGyro(18, speedSlow, 0, 3));
 
     // Swing turn to the left
-    addSequential(new Auto_BangTurnByGyro(.35, 0.09, -70));
+    addSequential(new Auto_BangTurnByGyro(outsidek, insidek, -70));
 
     // Drive forward
     addSequential(new Auto_BangDriveByGyro(12, speedMed, -70));
 
     // Swing turn to the right back to heading of 0
-    addSequential(new Auto_BangTurnByGyro(0.09, .35, 0));
+    addSequential(new Auto_BangTurnByGyro(insidek, outsidek, 0));
 
     // Drive fast and stright to the other end of the field
     addSequential(new Auto_BangDriveByGyro(98, speedFast, 0));
@@ -47,15 +49,15 @@ public class autoGroupBang extends CommandGroup {
 
     //loop
    
-    addSequential(new Auto_BangTurnByGyro(0.35, .09, 0));
-    addSequential(new Auto_BangTurnByGyro(0.35, .09, -90));;
-    addSequential(new Auto_BangTurnByGyro(0.35, .09, 180));
-    addSequential(new Auto_BangTurnByGyro(.35, .09, 90));
+    addSequential(new Auto_BangTurnByGyro(outsidek, insidek, 0));
+    addSequential(new Auto_BangTurnByGyro(outsidek, insidek, -90));;
+    addSequential(new Auto_BangTurnByGyro(outsidek, insidek, 180));
+    addSequential(new Auto_BangTurnByGyro(outsidek, insidek, 90));
     //addSequential(new Auto_SetNavX_Invert(true));
-    addSequential(new Auto_BangTurnByGyro(0.09, .35, 180));
+    addSequential(new Auto_BangTurnByGyro(insidek, outsidek, 180));
     addSequential(new Auto_BangDriveByGyro(95, speedFast, 180));
-    addSequential(new Auto_BangTurnByGyro(0.09, .35, -90));
-    addSequential(new Auto_BangTurnByGyro(0.35, .09, 180));
+    addSequential(new Auto_BangTurnByGyro(insidek, outsidek, -90));
+    addSequential(new Auto_BangTurnByGyro(outsidek, insidek, 180));
     
     // Stop NOW to advoid disaster
     addSequential(new Auto_BangStop());
